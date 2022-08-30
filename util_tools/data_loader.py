a7986e4dbe283a5b1f1897f5f37061269a6b06de
WML <menglinw@usc.edu>
2022-08-30 15:54:40 -0700

    debuged data load function, finished unify MERRA2 dimension

--- util_tools/data_loader.py
+++ util_tools/data_loader.py
@@ -11,17 +11,11 @@ import pandas as pd
 
 
 class data_processer():
-    def __init__(self, file_path_g_05, file_path_g_06, file_path_m, file_path_ele, file_path_country,
-                 test_proportion, n_lag, target_variable):
+    def __init__(self,test_proportion, n_lag):
         self.test_proportion = test_proportion
         self.n_lag = int(n_lag)
-        self.target_var = target_variable
-        self.file_path_g = [file_path_g_05, file_path_g_06]
-        self.file_path_m = file_path_m
-        self.file_path_ele = file_path_ele
-        self.load_data()
 
-    def load_data(self):
+    def load_data(self, target_variable, file_path_g_05, file_path_g_06, file_path_m, file_path_ele, file_path_country):
         # load country shape file
         country_shape = gpd.read_file(file_path_country[0])
         for country_path in file_path_country[1:]:
@@ -31,12 +25,13 @@ class data_processer():
         latmin, lonmin, latmax, lonmax = country_shape.total_bounds
 
         # load G5NR, log, normalize
-        g05_data = nc.Dataset(self.file_path_g[0])
-        g06_data = nc.Dataset(self.file_path_g[1])
-        g_data = np.concatenate((self._data_g5nr_to_array(g05_data), self._data_g5nr_to_array(g06_data)), axis=0)
+        g05_data = nc.Dataset(file_path_g_05)
+        g06_data = nc.Dataset(file_path_g_06)
+        g_data = np.concatenate((self.data_g5nr_to_array(g05_data, target_var=target_variable),
+                                 self.data_g5nr_to_array(g06_data, target_var=target_variable)), axis=0)
         # log and normalize
         g_data = np.log(g_data)
-        g_data = (g_data - g_data.min())/(np.quantile(g_data, 0.95) - g_data.min())
+        g_data = (g_data - g_data.min()) / (np.quantile(g_data, 0.95) - g_data.min())
         # cut G5NR data
         G_lats = g05_data.variables['lat'][:]
         G_lons = g05_data.variables['lon'][:]
@@ -44,44 +39,67 @@ class data_processer():
         latmax_ind = np.argmin(np.abs(G_lats - latmax))
         lonmin_ind = np.argmin(np.abs(G_lons - lonmin))
         lonmax_ind = np.argmin(np.abs(G_lons - lonmax))
-        self.g_data = g_data[:, latmin_ind - 1:latmax_ind + 1, lonmin_ind:lonmax_ind + 2]
+        g_data = g_data[:, latmin_ind - 1:latmax_ind + 1, lonmin_ind:lonmax_ind + 2]
         # load lat&lon of G5NR
-        self.G_lats = g05_data.variables['lat'][latmin_ind - 1:latmax_ind + 1]
-        self.G_lats = self._normalize(self.G_lats)
-        self.G_lons = g05_data.variables['lon'][lonmin_ind:lonmax_ind + 2]
-        self.G_lons = self._normlize(self.G_lons)
+        G_lats = g05_data.variables['lat'][latmin_ind - 1:latmax_ind + 1]
+        G_lats = self.normalize(G_lats)
+        G_lons = g05_data.variables['lon'][lonmin_ind:lonmax_ind + 2]
+        G_lons = self.normalize(G_lons)
+
+        # load Elevation
+        ele_data = np.load(file_path_ele)
+        ele_data = ele_data[latmin_ind - 1:latmax_ind + 1, lonmin_ind:lonmax_ind + 2]
 
         # load MERRA2, log, normalize
-        m_data = nc.Dataset(self.file_path_m)
-        self.m_data = m_data.variables[self.target_var][:, :, :]
+        m_ncdata = nc.Dataset(file_path_m)
+        m_data = m_ncdata.variables[target_variable][:, :, :]
         # log and normalize
-        self.m_data = np.log(self.m_data)
-        self.m_data = (self.m_data - self.m_data.min())/(np.quantile(self.m_data, 0.95) - self.m_data.min())
+        m_data = np.log(m_data)
+        m_data = (m_data - m_data.min()) / (np.quantile(m_data, 0.95) - m_data.min())
         # cut MERRA2 data
-        M_lats = m_data.variables['lat'][:]
-        M_lons = m_data.variables['lon'][:]
+        M_lats = m_ncdata.variables['lat'][:]
+        M_lons = m_ncdata.variables['lon'][:]
         latmin_ind = np.argmin(np.abs(M_lats - latmin))
         latmax_ind = np.argmin(np.abs(M_lats - latmax))
         lonmin_ind = np.argmin(np.abs(M_lons - lonmin))
         lonmax_ind = np.argmin(np.abs(M_lons - lonmax))
-        self.m_data = self.m_data[:, latmin_ind - 1:latmax_ind + 1, lonmin_ind:lonmax_ind + 2]
+        m_data = m_data[:, latmin_ind - 1:latmax_ind + 1, lonmin_ind:lonmax_ind + 2]
         # load lat&lon of MERRA2
-        self.M_lats = m_data.variables['lat'][latmin_ind - 1:latmax_ind + 1]
-        self.M_lats = self._normlize(self.M_lats)
-        self.M_lons = m_data.variables['lon'][lonmin_ind:lonmax_ind + 2]
-        self.M_lons = self._normlize(self.M_lons)
+        M_lats = m_ncdata.variables['lat'][latmin_ind - 1:latmax_ind + 1]
+        M_lats = self.normalize(M_lats)
+        M_lons = m_ncdata.variables['lon'][lonmin_ind:lonmax_ind + 2]
+        M_lons = self.normalize(M_lons)
 
-        # load Elevation
-        self.ele_data = np.load(self.file_path_ele)
+        return g_data, m_data, [G_lats, G_lons, M_lats, M_lons], ele_data
 
-    def _normlize(self, data):
-        return (data-data.min())/(data.max() - data.min())
+    def normalize(self, data):
+        return (data - data.min()) / (data.max() - data.min())
 
-    def _data_g5nr_to_array(self, nc_data, time_start=0, time_length=365):
+    def data_g5nr_to_array(self, nc_data, target_var, time_start=0, time_length=365):
         time_interval = range(time_start, time_start + time_length)
-        out = nc_data.variables[self.target_var][:][time_interval, :, :]
+        out = nc_data.variables[target_var][:][time_interval, :, :]
         return out
 
+    def unify_m_data(self, g_data, m_data, G_lats, G_lons, M_lats, M_lons):
+        lat_dim, lon_dim = g_data.shape[1:]
+        unif_m_data = np.zeros((m_data.shape[0], lat_dim, lon_dim))
+        for i in range(lat_dim):
+            for j in range(lon_dim):
+                lat = G_lats[i]
+                lon = G_lons[j]
+                m_lat_idx = np.argmin(np.abs(M_lats - lat))
+                m_lon_idx = np.argmin(np.abs(M_lons - lon))
+                unif_m_data[:, i, j] = m_data[:, m_lat_idx, m_lon_idx]
+        return unif_m_data
+
+        
+    def data_process(self):
+        # TODO: unify dimension
+        self.unify_m_data()
+        # TODO: split tran/test
+        # TODO: write flatten function
+        # TODO: cache data
+        pass
 
 if __name__=="__main__":
     start = time.time()
