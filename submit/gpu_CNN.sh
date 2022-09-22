61daf0a0d72210773b0044ed646386fc2e6ed8a6
WML <menglinw@usc.edu>
2022-09-22 15:07:26 -0700

    batch script

---
+++ submit/gpu_CNN.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --mem=32GB
+#SBATCH --time=48:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --account=mereditf_284
+
+
+module load gcc/8.3.0
+module load intel/19.0.4
+module load cuda/10.2.89
+module load anaconda3
+eval "$(conda shell.bash hook)"
+conda activate cGAN_space
+python3 country_final_model.py 1 without_trans without_merra forward BCSMASS 2
