58715be3e3a535a78fe5142d09dd361f47491279
WML <menglinw@usc.edu>
2022-09-27 15:32:06 -0700

    start batch train

---
+++ submit/batch_run_model.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --mem=64GB
+#SBATCH --time=48:00:00
+#SBATCH --account=mereditf_284
+
+
+module load gcc/8.3.0
+module load intel/19.0.4
+module load cuda/10.2.89
+module load anaconda3
+eval "$(conda shell.bash hook)"
+conda activate cGAN_space
--- submit/train_model.sh
+++ submit/train_model.sh
@@ -6,7 +6,7 @@ for season in 1 2 3 4
 do
   for area in 1 2 3 4
   do
-    cp batch_run.sh $save_path/Season$season/Area$area/batch_run_model.sh
+    cp batch_run_model.sh $save_path/Season$season/Area$area
     cd $save_path/Season$season/Area$area
     echo "python3 /scratch1/menglinw/S2S_Downscaling/train_model.py $save_path/Season$season/Area$area">>batch_run_model.sh
     echo "python3 /scratch1/menglinw/S2S_Downscaling/train_model.py $save_path/Season$season/Area$area"
