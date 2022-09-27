9e160758d32e540ce91a322c7baa068666e66040
WML <menglinw@usc.edu>
2022-09-26 19:06:17 -0700

    cGAN example code

--- submit/gpu_CNN.sh
+++ submit/gpu_CNN.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --mem=128GB
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 #SBATCH --account=mereditf_284
 
 
@@ -11,4 +11,4 @@ module load cuda/10.2.89
 module load anaconda3
 eval "$(conda shell.bash hook)"
 conda activate cGAN_space
-python3 ../main.py
+python3 ../get_data_ready.py /scratch1/menglinw/S2S_temp_data/Season1/Area1/ 1 1
