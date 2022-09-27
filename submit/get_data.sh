5a9052bb40b90daee93f75ced599ae6ea00d1379
WML <menglinw@usc.edu>
2022-09-26 22:38:19 -0700

    batch run

--- submit/get_data.sh
+++ submit/get_data.sh
@@ -8,8 +8,8 @@ do
   for area in 1 2 3 4
   do
     mkdir $save_path/Season$season/Area$area
-    cp batch_run.sh $save_path/$season/$area
-    cd $save_path/$season/$area
+    cp batch_run.sh $save_path/Season$season/Area$area
+    cd $save_path/Season$season/Area$area
     echo "python3 /scratch1/menglinw/S2S_Downscaling/get_data_ready.py $save_path/Season$season/Area$area $season $area">>batch_run.sh
     echo "python3 /scratch1/menglinw/S2S_Downscaling/get_data_ready.py $save_path/Season$season/Area$area $season $area"
     sbatch batch_run.sh
