dcb1adce35acc5e4c049d4af3c240c48e9df6af6
WML <menglinw@usc.edu>
2022-10-09 21:19:33 -0700

    start debuging cGAN fine tune

--- train_model.py
+++ train_model.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     data_cache_path = sys.argv[1]
     n_lag = 15
     n_pred = 1
-    task_dim = [15, 15]
+    task_dim = [8, 8]
 
     # load data
     X_high = np.load(os.path.join(data_cache_path, 'X_high.npy'))
