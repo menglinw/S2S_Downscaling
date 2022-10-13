a019da68fee2f576795dcd0c9d8fcee966777de4
WML <menglinw@usc.edu>
2022-10-13 08:52:00 -0700

    update evaluate script

--- evaluate_downscale_result.py
+++ evaluate_downscale_result.py
@@ -2,6 +2,8 @@ import numpy as np
 import os
 import sys
 from scipy import stats
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
 
 def reconstruc_downscale(data1, data2, data3, data4):
     data_left = np.concatenate([data1, data2], axis=1)
@@ -22,10 +24,8 @@ def read_lon_data(path, lat_id):
     d_data3, t_data3 = read_sub_data(os.path.join(path, 'Area' + str(((lat_id - 1) * 7) + 3)))
     d_data4, t_data4 = read_sub_data(os.path.join(path, 'Area' + str(((lat_id - 1) * 7) + 4)))
     d_data5, t_data5 = read_sub_data(os.path.join(path, 'Area' + str(((lat_id - 1) * 7) + 5)))
-    d_data6, t_data6 = read_sub_data(os.path.join(path, 'Area' + str(((lat_id - 1) * 7) + 6)))
-    d_data7, t_data7 = read_sub_data(os.path.join(path, 'Area' + str(((lat_id - 1) * 7) + 7)))
-    d_data = np.concatenate([d_data1, d_data2, d_data3, d_data4, d_data5, d_data6, d_data7], axis=2)
-    t_data = np.concatenate([t_data1, t_data2, t_data3, t_data4, t_data5, t_data6, t_data7], axis=2)
+    d_data = np.concatenate([d_data1, d_data2, d_data3, d_data4, d_data5], axis=2)
+    t_data = np.concatenate([t_data1, t_data2, t_data3, t_data4, t_data5], axis=2)
     return d_data, t_data
 
 
@@ -34,9 +34,8 @@ def read_downscale_data(data_cache_path, season):
     d_data1, t_data1 = read_lon_data(os.path.join(data_cache_path, season), 1)
     d_data2, t_data2 = read_lon_data(os.path.join(data_cache_path, season), 2)
     d_data3, t_data3 = read_lon_data(os.path.join(data_cache_path, season), 3)
-    d_data4, t_data4 = read_lon_data(os.path.join(data_cache_path, season), 4)
-    d_data = np.concatenate([d_data1, d_data2, d_data3, d_data4], axis=1)
-    t_data = np.concatenate([t_data1, t_data2, t_data3, t_data4], axis=1)
+    d_data = np.concatenate([d_data1, d_data2, d_data3], axis=1)
+    t_data = np.concatenate([t_data1, t_data2, t_data3], axis=1)
     return d_data, t_data
 
 
@@ -47,13 +46,19 @@ def rsquared(x, y):
 
 def evaluate(d_data, t_data):
     n_lag = t_data.shape[0] - d_data.shape[0]
-    print('Day: ', 'R2 w/o fine tune')
+    print('Day: ', 'R2', 'RMSE')
+    r2_list = []
+    rmse_list = []
     for i in range(d_data.shape[0]):
         r2, p = rsquared(t_data[n_lag+i].reshape(np.prod(t_data.shape[1:])),
                          d_data[i].reshape(np.prod(d_data.shape[1:])))
+        rmse = np.sqrt(np.mean(np.square(t_data[n_lag+i] - d_data[i])))
         #r2_f, p = rsquared(t_data[n_lag+i].reshape(np.prod(t_data.shape[1:])),
         #                 df_data[i].reshape(np.prod(df_data.shape[1:])))
-        print('Day', i+1, ': ',  r2)
+        print('Day', i+1, ': ',  r2, rmse)
+        r2_list.append(r2)
+        rmse_list.append(rmse)
+    print('Avg:', np.mean(r2_list), np.mean(rmse_list))
 
 
 def read_and_save(data_cache_path):
