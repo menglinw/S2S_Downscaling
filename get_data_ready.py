a2cae509187006da933c82993e3b9efecd28acc9
WML <menglinw@usc.edu>
2022-12-06 15:11:11 -0800

    test data processing together

--- get_data_ready.py
+++ get_data_ready.py
@@ -18,7 +18,7 @@ def id_to_boundary(id):
         return 225, 360
 
 
-def get_data(data_cache_path, target_var, n_lag, n_pred, task_dim, test_ratio, season, area, AFG_only=False, stride=2):
+def get_data(data_cache_path, target_var, n_lag, n_pred, task_dim, train_set, area, AFG_only=False, stride=2):
     start = time.time()
     if not os.path.exists(data_cache_path):
         os.makedirs(data_cache_path, exist_ok=True)
@@ -68,21 +68,6 @@ def get_data(data_cache_path, target_var, n_lag, n_pred, task_dim, test_ratio, s
         G_lats = G_lats[lat_low:lat_high]
         G_lons = G_lons[lon_low:lon_high]
 
-    # season subset
-    year1_avlb_days = list(range((season - 1) * 91 + n_lag, season * 91 if season != 4 else season * 91 + 1))
-    year2_avlb_days = list(range(365 + (season - 1) * 91 + n_lag,
-                                 365 + season * 91 if season != 4 else 365 + season * 91 + 1))
-    avlb_days = year1_avlb_days + year2_avlb_days
-
-
-    # train/test split
-    # get all available days
-    # random split into train/test sets
-    avlb_days = set(avlb_days)
-    test_set = set(random.sample(avlb_days, int(len(avlb_days)*test_ratio)))
-    train_set = avlb_days - test_set
-    # save test date
-    np.save('test_days.npy', np.array(list(test_set)))
     # flatten train data (day by day)
     if 'X_high.npy' not in os.listdir(data_cache_path) or \
             'X_low.npy' not in os.listdir(data_cache_path) or \
@@ -131,9 +116,45 @@ if __name__ == '__main__':
     target_var = 'DUEXTTAU'
     test_ratio = 0.1
     data_cache_path = sys.argv[1]
-    season = int(sys.argv[2])
-    area = int(sys.argv[3])
-    AFG_only = True if sys.argv[4] == 'AFG' else False
+    #season = int(sys.argv[2])
+    #area = int(sys.argv[3])
+    #AFG_only = True if sys.argv[4] == 'AFG' else False
 
-    get_data(data_cache_path, target_var, n_lag, n_pred, task_dim, test_ratio, season, area, AFG_only=AFG_only,
-             stride=stride)
+    for season in [1, 2, 3, 4]:
+        # season subset
+        year1_avlb_days = list(
+            range((season - 1) * 91 + n_lag if (season - 1) * 91 - 45 < 0 else (season - 1) * 91 + n_lag - 45,
+                  season * 91 if season != 4 else season * 91 + 1))
+        year2_avlb_days = list(range(365 + (season - 1) * 91 + n_lag - 45,
+                                     365 + season * 91 if season != 4 else 365 + season * 91 + 1))
+        avlb_days = year1_avlb_days + year2_avlb_days
+
+        # train/test split
+        # get all available days
+        # random split into train/test sets
+        avlb_days = set(avlb_days)
+        test_set = set(random.sample(avlb_days, int(len(avlb_days) * test_ratio)))
+        train_set = avlb_days - test_set
+
+        # save test date
+        np.save(os.path.join(data_cache_path, 'test_days.npy'), np.array(list(test_set)))
+
+        # create season dir if not exist
+        season_path = os.path.join(data_cache_path, 'Season'+str(season))
+        if not os.path.exists(season_path):
+            os.mkdir(season_path)
+
+        # create area dir and data
+        for area in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+            # create area dir
+            area_path = os.path.join(season_path, 'Area'+str(area))
+            if not os.path.exists(area_path):
+                os.mkdir(area_path)
+
+            AFG_only = True if area == 0 else False
+            # create area dataset
+            get_data(area_path, target_var, n_lag, n_pred, task_dim, train_set, area, AFG_only=AFG_only,
+                     stride=stride)
+
+
+
--- get_downscale_ready.py
+++ get_downscale_ready.py
@@ -7,6 +7,31 @@ if '..' not in sys.path:
     sys.path.append('..')
 import util_tools
 from util_tools import downscale
+from tensorflow.python.framework.ops import disable_eager_execution
+disable_eager_execution()
+
+# define helper function
+def mapping_tanh(x):
+    x02 = tf.keras.backend.tanh(x) + 1  # x in range(0,2)
+    scale = 1 / 2.
+    return x02 * scale
+
+
+def mapping_abs(x):
+    x02 = x / (1 + tf.keras.backend.abs(x)) + 1
+    scale = 1 / 2.
+    return x02 * scale
+
+
+def nnelu(x):
+    return tf.add(tf.constant(1, dtype=tf.float32), tf.nn.elu(x))
+
+
+def sampling(mu_log_variance):
+    mu, log_variance = mu_log_variance
+    epsilon = tf.keras.backend.random_normal(shape=tf.keras.backend.shape(mu), mean=0.0, stddev=1.0)
+    random_sample = mu + tf.keras.backend.exp(log_variance / 2) * epsilon
+    return random_sample
 
 
 def loss_func(encoder_mu, encoder_log_variance):
@@ -15,7 +40,7 @@ def loss_func(encoder_mu, encoder_log_variance):
         reconstruction_loss = tf.keras.backend.mean(tf.keras.backend.abs(y_true - y_predict), axis=[1, 2, 3])
         return reconstruction_loss_factor * reconstruction_loss
 
-    def vae_kl_loss(encoder_mu, encoder_log_variance):
+    def vae_kl_loss(y_true, y_predict):
         kl_loss = -0.5 * tf.keras.backend.sum(1.0 + encoder_log_variance - tf.keras.backend.square(encoder_mu) -
                                               tf.keras.backend.exp(encoder_log_variance), axis=1)
         return kl_loss
@@ -34,36 +59,95 @@ def loss_func(encoder_mu, encoder_log_variance):
 
     return vae_loss
 
+def get_generator(n_lag, n_pred, task_dim, latent_space_dim):
+    high_input = tf.keras.Input(shape=(n_lag, task_dim[0], task_dim[1], 1))
+
+    x1 = tf.keras.layers.ConvLSTM2D(32, kernel_size=(3, 3), return_sequences=False,
+                                    activation=tf.keras.layers.LeakyReLU())(high_input)
+    x1 = tf.keras.layers.Flatten()(x1)
+
+    low_input = tf.keras.Input(shape=(n_lag, task_dim[0], task_dim[1], 1))
+    x2 = tf.keras.layers.ConvLSTM2D(16, kernel_size=(3, 3), return_sequences=False,
+                                    activation=tf.keras.layers.LeakyReLU())(low_input)
+    x2 = tf.keras.layers.Flatten()(x2)
+
+    ele_input = tf.keras.Input(shape=(task_dim[0], task_dim[1], 1))
+    x3 = tf.keras.layers.Dense(16, activation=tf.keras.layers.LeakyReLU())(ele_input)
+    x3 = tf.keras.layers.Flatten()(x3)
+
+    other_input = tf.keras.Input(shape=(3))
+    x4 = tf.keras.layers.Dense(8, activation=tf.keras.layers.LeakyReLU())(other_input)
+
+    e1 = tf.keras.layers.Concatenate(axis=1)([x1, x2, x3, x4])
+    e2 = tf.keras.layers.Dense(30, kernel_initializer="he_normal", use_bias=True,
+                               kernel_regularizer=tf.keras.regularizers.l2(0.01),
+                               activation=tf.keras.layers.LeakyReLU())(e1)
+    e3 = tf.keras.layers.BatchNormalization()(e2)
+    e3 = tf.keras.layers.Dropout(0.5)(e3)
+    e4 = tf.keras.layers.Dense(30, kernel_initializer="he_normal", use_bias=True,
+                               kernel_regularizer=tf.keras.regularizers.l2(0.01),
+                               activation=tf.keras.layers.LeakyReLU())(e3)
+    e5 = tf.keras.layers.BatchNormalization()(e4)
+    e5 = tf.keras.layers.Dropout(0.5)(e5)
+    e6 = tf.keras.layers.Dense(30, kernel_initializer="he_normal", use_bias=True,
+                               kernel_regularizer=tf.keras.regularizers.l2(0.01),
+                               activation=tf.keras.layers.LeakyReLU())(e5)
+    e7 = tf.keras.layers.BatchNormalization()(e6)
+    e7 = tf.keras.layers.Dropout(0.5)(e7)
+    est_mu = tf.keras.layers.Dense(units=latent_space_dim, name="encoder_mu")(e7)
+    est_log_variance = tf.keras.layers.Dense(units=latent_space_dim, name="encoder_log_variance")(e7)
+    est_output = tf.keras.layers.Lambda(sampling, name="encoder_output")([est_mu, est_log_variance])
+
+    d1 = tf.keras.layers.Dense(30, kernel_initializer="he_normal", use_bias=True,
+                               kernel_regularizer=tf.keras.regularizers.l2(0.01),
+                               activation=tf.keras.layers.LeakyReLU())(est_output)
+    d2 = tf.keras.layers.BatchNormalization()(d1)
+    d3 = tf.keras.layers.Concatenate(axis=1)([d2, e7])
+    d3 = tf.keras.layers.Dropout(0.5)(d3)
+    d4 = tf.keras.layers.Dense(30, kernel_initializer="he_normal", use_bias=True,
+                               kernel_regularizer=tf.keras.regularizers.l2(0.01),
+                               activation=tf.keras.layers.LeakyReLU())(d3)
+    d5 = tf.keras.layers.BatchNormalization()(d4)
+    d6 = tf.keras.layers.Concatenate(axis=1)([d5, e5])
+    d6 = tf.keras.layers.Dropout(0.5)(d6)
+    d7 = tf.keras.layers.Dense(30, kernel_initializer="he_normal", use_bias=True,
+                               kernel_regularizer=tf.keras.regularizers.l2(0.01),
+                               activation=tf.keras.layers.LeakyReLU())(d6)
+    d8 = tf.keras.layers.BatchNormalization()(d7)
+    d9 = tf.keras.layers.Concatenate(axis=1)([d8, e3])
+    d9 = tf.keras.layers.Dropout(0.5)(d9)
+    d10 = tf.keras.layers.Dense(30, kernel_initializer="he_normal", use_bias=True,
+                                kernel_regularizer=tf.keras.regularizers.l2(0.01),
+                                activation=tf.keras.layers.LeakyReLU())(d9)
+    d11 = tf.keras.layers.BatchNormalization()(d10)
+    d12 = tf.keras.layers.Dense(n_pred * np.prod(task_dim), activation=mapping_abs)(d11)
+    d13 = tf.keras.layers.Reshape([n_pred, task_dim[0], task_dim[1]])(d12)
+    generator = tf.keras.Model([high_input, low_input, ele_input, other_input], d13)
+    opt = tf.keras.optimizers.Adam(learning_rate=0.0005)
+    generator.compile(optimizer=opt, loss=loss_func(est_mu, est_log_variance))
+    return generator
+
 
 # define parameters
 data_cache_path = sys.argv[1]
 n_lag = 20
-n_pred = 10
-task_dim = [1, 1]
+n_pred = 1
+task_dim = [5, 5]
+latent_space_dim = 50
 start = time.time()
 
+# load model (need to test)
+generator = get_generator(n_lag, n_pred, task_dim, latent_space_dim)
+generator.load_weights('s2s_model')
+
 # load data
-test_g_data = np.load(os.path.join(data_cache_path, 'test_g_data.npy'))
-test_m_data = np.load(os.path.join(data_cache_path, 'test_m_data.npy'))
-test_days = np.load(os.path.join(data_cache_path, 'test_days.npy'))
-ele_data = np.load(os.path.join(data_cache_path, 'test_ele.npy'))
-G_lats = np.load(os.path.join(data_cache_path, 'test_lats.npy'))
-G_lons = np.load(os.path.join(data_cache_path, 'test_lons.npy'))
-M_lats = None
-M_lons = None
 
-# load model
-generator = tf.keras.models.load_model(os.path.join(data_cache_path, 's2s_model'))
-#generator_fine = tf.keras.models.load_model(os.path.join(data_cache_path, 's2s_model_fine'))
-
-# run downscale
+# run downscale on test set
 dscler = downscale.downscaler(generator)
 downscaled_data = dscler.downscale(test_g_data[:n_lag], test_m_data, ele_data,  [G_lats, G_lons, M_lats, M_lons],
                                    test_days, n_lag, n_pred, task_dim)
 
-#dscler_fine = downscale.downscaler(generator_fine)
-#downscaled_data_fine = dscler_fine.downscale(test_g_data[:n_lag], test_m_data, ele_data,  [G_lats, G_lons, M_lats, M_lons],
-#                                   test_days, n_lag, n_pred, task_dim)
+
 # save downscale data
 np.save(os.path.join(data_cache_path, 'downscaled_data.npy'), downscaled_data)
 #np.save(os.path.join(data_cache_path, 'downscaled_data_fine.npy'), downscaled_data_fine)
