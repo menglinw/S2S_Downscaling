cafe18c3d15fe3d493664311a8f55730c265fbff
WML <menglinw@usc.edu>
2022-12-27 21:40:57 -0800

    test cov evaluate environment

--- Cov_evaluate.py
+++ Cov_evaluate.py
@@ -1,5 +1,18 @@
 from skgstat import Variogram, OrdinaryKriging
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.stattools import adfuller
+import numpy as np
+import sys
+import os
+import tensorflow as tf
+import time
+if '..' not in sys.path:
+    sys.path.append('..')
+from util_tools.data_loader import data_processer
+import pandas as pd
+from util_tools import downscale
+import pandas as pd
+from scipy import stats
+
 
 print('Successfully loaded packages!')
